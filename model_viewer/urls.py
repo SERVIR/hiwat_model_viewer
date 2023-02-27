fd5c8f79fce23599be80b9029f4b262c75ecbb7e
BillyZ313 <billy.ashmall@nasa.gov>
2023-02-27 15:34:25 -0600

    upgrade django

--- model_viewer/urls.py
+++ model_viewer/urls.py
@@ -1,13 +1,13 @@
 ﻿from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-from django.conf.urls import url
+
 
 from . import views
 
 urlpatterns = [
     path('ajax/getjson', views.getjson, name='getjson'),
     path('ajax/getimage', views.getimage, name='getimage'),
-    url('', views.index, name='index'),
+    path('', views.index, name='index'),
     # url('', TemplateView.as_view(template_name="model_viewer/index.html"), name='index'),
 ]
